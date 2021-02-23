5cae38f8da7266bd04906cf1194609d315838b33
Matthias Runte <matthias@runte.de>
2021-02-23 19:20:41 +0100

    (c) updated

--- MTC4PU/MTC4PU.ino
+++ MTC4PU/MTC4PU.ino
@@ -1,5 +1,5 @@
 // Author: Dr. Matthias Runte
-// Copyright 2020 by Dr. Matthias Runte
+// Copyright 2020, 2021 by Dr. Matthias Runte
 // License:
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
