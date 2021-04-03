5847e04d9759d81039a2dc7bd65e7e4be7c7ff81
Raymond Brink <git@rbrink.nl>
2021-04-04 00:01:21 +0200

    + Added MTC4BT config example.

--- MTC4BT/include/MTC4BT_loco_config.example.h
+++ MTC4BT/include/MTC4BT_loco_config.example.h
@@ -8,7 +8,8 @@
 #define AUTO_LIGHTS_ENABLED true
 #define AUTO_LIGHTS_DISABLED false
 
-#define numHubs 4
+// Configure the total number of SBrick/PU Hubs you want to use.
+#define numHubs 2
 BLEHub *hubs[numHubs];
 
 // BLE scan duration in seconds. If the device isn't found within this timeframe the scan is aborted.
@@ -17,7 +18,7 @@ const uint32_t BLE_SCAN_DURATION_IN_SECONDS = 1;
 // Duration between BLE discovery and connect attempts in seconds.
 const uint32_t BLE_CONNECT_DELAY_IN_SECONDS = 2;
 
-// Until we get this to work, we can't connect more than 3 hubs (even when we configure more below):
+// Until we get this to work, we can't connect to more than 3 hubs (even when we configure more below):
 //  https://github.com/h2zero/NimBLE-Arduino/issues/99#issuecomment-667213152
 //  The solution is actually a lot simpler. Just change the defines in the sdkconfig.h file at Arduino/hardware/espressif/esp32/tools/sdk/include/config to:
 //  #define CONFIG_BTDM_CONTROLLER_BLE_MAX_CONN 9
@@ -36,18 +37,4 @@ void configureHubs()
     channels_PT60197.push_back({HubChannel::A, AttachedDevice::LIGHT, HubChannelDirection::FORWARD, 5, 10});
     channels_PT60197.push_back({HubChannel::B, AttachedDevice::MOTOR, HubChannelDirection::FORWARD, 5, 10});
     hubs[1] = new PUHub("PT60197", "90:84:2b:07:13:7f", &channels_PT60197, 80);
-
-    std::vector<ChannelConfiguration> channels_BC60052;
-    channels_BC60052.push_back({HubChannel::A});
-    channels_BC60052.push_back({HubChannel::B, AttachedDevice::MOTOR, HubChannelDirection::FORWARD, 10, 20});
-    channels_BC60052.push_back({HubChannel::C});
-    channels_BC60052.push_back({HubChannel::D, AttachedDevice::LIGHT, HubChannelDirection::FORWARD, 10, 20});
-    hubs[2] = new SBrickHub("BC60052", "88:6b:0f:23:78:10", &channels_BC60052, 80);
-
-    std::vector<ChannelConfiguration> channels_HE10233;
-    channels_HE10233.push_back({HubChannel::A, AttachedDevice::LIGHT, HubChannelDirection::FORWARD, 10, 20});
-    channels_HE10233.push_back({HubChannel::B});
-    channels_HE10233.push_back({HubChannel::C, AttachedDevice::MOTOR, HubChannelDirection::FORWARD, 10, 20});
-    channels_HE10233.push_back({HubChannel::D});
-    hubs[3] = new SBrickHub("HE10233", "00:07:80:d0:3a:f2", &channels_HE10233, 80);
 }
