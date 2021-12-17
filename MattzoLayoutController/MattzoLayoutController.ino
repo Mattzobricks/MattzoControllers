c75e5bd3e301f611fbb9a14036970dbf717963c1
Matthias Runte <matthias@runte.de>
2021-12-17 08:48:16 +0100

    MLC Bugfix

--- MattzoLayoutController/MattzoLayoutController.ino
+++ MattzoLayoutController/MattzoLayoutController.ino
@@ -9,7 +9,7 @@
 #define MATTZO_CONTROLLER_TYPE "MattzoLayoutController"
 #include <ESP8266WiFi.h>                          // WiFi library for ESP-8266
 #include <Servo.h>                                // Servo library
-#include "MattzoLayoutController_Configuration_BasculeBridge.h" // this file should be placed in the same folder
+#include "MattzoLayoutController_Configuration.h" // this file should be placed in the same folder
 #include "MattzoController_Library.h"             // this file needs to be placed in the Arduino library folder
 
 #if USE_PCA9685
@@ -676,7 +676,7 @@ void setServoAngle(int servoIndex, int servoAngle) {
       mattzoServo[servoIndex].lastSwitchingAction_ms = millis();
     }
 #if USE_PCA9685
-    else if (SWITCHPORT_PIN_TYPE[servoIndex] >= 0x40) {
+    else if (servoConfiguration[servoIndex].pinType >= 0x40) {
       setPCA9685SleepMode(false);
       pca9685[servoConfiguration[servoIndex].pinType - 0x40].setPWM(servoConfiguration[servoIndex].pin, 0, mapAngle2PulseLength(servoAngle));
     }
