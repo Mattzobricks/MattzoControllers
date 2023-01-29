511d15b9be4c2855a0f6f5705b80a42410ae172c
Hilbert70 <hilbert70@gmail.com>
2023-01-29 22:03:15 +0100

    Removed all code related to the internal queue, it is not needed. MQTT messages have prio 1

--- src/lib/MCNetwork/MattzoMQTTSubscriber.cpp
+++ src/lib/MCNetwork/MattzoMQTTSubscriber.cpp
@@ -6,7 +6,7 @@
 WiFiClient wifiSubscriberClient;
 PubSubClient mqttSubscriberClient(wifiSubscriberClient);
 
-void MattzoMQTTSubscriber::Setup(MCMQTTConfiguration *config, void (*handleMQTTMessageLoop)(void *parm))
+void MattzoMQTTSubscriber::Setup(MCMQTTConfiguration *config, void (*mqttcallback)(char *topic, byte *payload, unsigned int length))
 {
 #if !defined(ESP32)
 #error "Error: this sketch is designed for ESP32 only."
@@ -20,62 +20,26 @@ void MattzoMQTTSubscriber::Setup(MCMQTTConfiguration *config, void (*handleMQTTM
     }
 
     // Setup a queue with a fixed length that will hold pointers to incoming MQTT messages.
-    IncomingQueue = xQueueCreate(MQTT_INCOMING_QUEUE_LENGTH, sizeof(char *));
+    // IncomingQueue = xQueueCreate(MQTT_INCOMING_QUEUE_LENGTH, sizeof(char *));
 
     // Setup MQTT client.
     log4MC::vlogf(LOG_INFO, "MQTT: Connecting to %s:%u...", _config->ServerAddress.c_str(), _config->ServerPort);
     mqttSubscriberClient.setServer(_config->ServerAddress.c_str(), _config->ServerPort);
     mqttSubscriberClient.setKeepAlive(_config->KeepAlive);
     mqttSubscriberClient.setBufferSize(MaxBufferSize);
-    mqttSubscriberClient.setCallback(mqttCallback);
+    mqttSubscriberClient.setCallback(mqttcallback);
 
     // Construct subscriber name.
     strcpy(_subscriberName, _config->SubscriberName);
     strcat(_subscriberName, "Subscriber");
 
-    // Start MQTT task loop to handle queued messages.
-    xTaskCreatePinnedToCore(handleMQTTMessageLoop, "MQTTHandler", MQTT_TASK_STACK_DEPTH, NULL, MQTT_TASK_PRIORITY, NULL, MQTT_HANDLE_MESSAGE_TASK_COREID);
-
     // Setup completed.
     _setupCompleted = true;
-
-    // Start task loop.
-    // uncomment if you DO NOT CALL loop in the main arduino loop!!!!!!
-    // xTaskCreatePinnedToCore(taskLoop, "MQTTSubscriber", StackDepth, NULL, TaskPriority, NULL, CoreID);
 }
 
 int MattzoMQTTSubscriber::GetStatus()
 {
     return _setupCompleted ? mqttSubscriberClient.state() : MQTT_UNINITIALIZED;
-}
-
-void MattzoMQTTSubscriber::mqttCallback(char *topic, byte *payload, unsigned int length)
-{
-    // Allocate memory to hold the message.
-    char *message = (char *)malloc(length + 1);
-    for (int i = 0; i < length; i++) {
-        message[i] = (char)payload[i];
-        if (i == 4) {
-            // Check if this is a message we should ignore.
-            if ((strstr(message, "<sys ")) == nullptr &&
-                (strstr(message, "<lc ")) == nullptr &&
-                (strstr(message, "<fn ")) == nullptr) {
-                // Nothing we can handle, so ignore this message.
-                free(message);
-                return;
-            }
-        }
-    }
-    message[length] = '\0';
-
-    // Store pointer to message in queue (don't block if the queue is full).
-    if (xQueueSendToBack(IncomingQueue, (void *)&message, (TickType_t)0) == pdTRUE) {
-        // Serial.println("[" + String(xPortGetCoreID()) + "] Ctrl: Queued incoming MQTT message [" + String(topic) + "]: " + String(message));
-    } else {
-        // Free the allocated memory block as we couldn't queue the message anyway.
-        free(message);
-        log4MC::warn("MQTT: Incoming MQTT message queue full");
-    }
 }
 
 /// <summary>
@@ -118,7 +82,7 @@ void MattzoMQTTSubscriber::reconnect()
             log4MC::vlogf(LOG_WARNING, "MQTT: Subscriber connect failed, rc=%u. Try again in a few seconds...", mqttSubscriberClient.state());
 
             // Wait a litte while before retrying.
-            vTaskDelay(ReconnectDelayInMilliseconds / portTICK_PERIOD_MS);
+            delay(ReconnectDelayInMilliseconds);
         }
     }
 }
@@ -143,38 +107,10 @@ void MattzoMQTTSubscriber::loop()
     // Allow the MQTT client to process incoming messages and maintain its connection to the server.
     mqttSubscriberClient.loop();
 }
-/// <summary>
-/// The main (endless) task loop.
-/// </summary>
-/// <param name="parm"></param>
-void MattzoMQTTSubscriber::taskLoop(void *parm)
-{
-    try {
-        /* code */
-        if (!_setupCompleted) {
-            const char *message = "MQTT: Setup not completed. Execute .Setup() first.";
-            log4MC::error(message);
-            throw message;
-        }
-
-        // Loop forever.
-        for (;;) {
-
-            loop();
-            // Wait a while before trying again (allowing other tasks to do their work).
-            vTaskDelay(HandleMessageDelayInMilliseconds / portTICK_PERIOD_MS);
-        }
-    } catch (const std::exception &e) {
-        log4MC::vlogf(LOG_EMERG, "Caught exception: %s", e.what());
-    } catch (...) {
-        log4MC::fatal("Caught an unknown exception");
-    }
-}
 
 // Initialize static members.
 QueueHandle_t MattzoMQTTSubscriber::IncomingQueue = nullptr;
 int MattzoMQTTSubscriber::ReconnectDelayInMilliseconds = 1000;
-int MattzoMQTTSubscriber::HandleMessageDelayInMilliseconds = 10;
 uint8_t MattzoMQTTSubscriber::TaskPriority = 2;
 int8_t MattzoMQTTSubscriber::CoreID = 0;
 uint32_t MattzoMQTTSubscriber::StackDepth = 2048;
