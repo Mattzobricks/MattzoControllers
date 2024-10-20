8f6984be4e6b9a9428b129dfa196239f2ad93d16
Hilbert70 <hilbert70@gmail.com>
2024-10-20 16:39:00 +0200

    Update #81 cleanup, no clue why it is not working

--- src/MTC4BT/include/BuWizz2Hub.h
+++ src/MTC4BT/include/BuWizz2Hub.h
@@ -4,13 +4,13 @@
 
 #include "BLEHub.h"
 
-#define BUWIZZ2_REMOTECONTROL_SERVICE_UUID        "4e050000-74fb-4481-88b3-9919b1676e93"
+#define BUWIZZ2_REMOTECONTROL_SERVICE_UUID "4e050000-74fb-4481-88b3-9919b1676e93"
 #define BUWIZZ2_REMOTECONTROL_CHARACTERISTIC_UUID "000092d1-0000-1000-8000-00805f9b34fb"
 
 #define BUWIZZ2_MIN_SPEED_FORWARD 0
-#define BUWIZZ2_MAX_SPEED_FORWARD 127
-#define BUWIZZ2_MAX_SPEED_REVERSE 0
-#define BUWIZZ2_MIN_SPEED_REVERSE -127
+#define BUWIZZ2_MAX_SPEED_FORWARD 0x7f
+#define BUWIZZ2_MAX_SPEED_REVERSE 255
+#define BUWIZZ2_MIN_SPEED_REVERSE 0x81
 
 const int8_t SET_MOTOR_DATA = 0x10;
 
@@ -35,8 +35,6 @@ class BuWizz2Hub : public BLEHub
     void NotifyCallback(NimBLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify);
 
   private:
-    byte _hubLedPort;
-
     void parseDeviceSatusMessage(uint8_t *pData, size_t length);
     void writeValue(byte command[], int size);
 };
