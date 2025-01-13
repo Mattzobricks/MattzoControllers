ab448cecd7a6eead60cc057294a0197ed71c8d3f
Matthias Runte <matthias.runte@meinestadt.de>
2025-01-13 08:13:47 +0100

    #113 bug fix

--- src/MTC4BT/lib/MController/MCLocoTrigger.cpp
+++ src/MTC4BT/lib/MController/MCLocoTrigger.cpp
@@ -5,5 +5,5 @@ MCLocoTrigger::MCLocoTrigger(MCTriggerSource source, std::string eventType, std:
 
 bool MCLocoTrigger::Matches(MCTriggerSource source, std::string eventType, std::string eventId, std::string value)
 {
-	return _source == source && _eventType == eventType && _eventId == eventId && _value == value;
+	return _source == source && _eventType.compare(eventType) == 0 && _eventId.compare(eventId) == 0 && _value.compare(value) == 0;
 }
