5686fa022a1aa5f5c13ba1f3b2fb3c1c47db5c3c
Hilbert70 <hilbert70@gmail.com>
2025-01-28 22:00:24 +0100

    Changed the default DRIVETASKDELAY to 100

--- src/MTC4BT/include/DriverTaskDelay.h
+++ src/MTC4BT/include/DriverTaskDelay.h
@@ -1,13 +1,13 @@
 #pragma once
 /*
-The default value is 250 and may be overridded by a -D flag, only change if you know what you
+The default value is 100 and may be overridded by a -D flag, only change if you know what you
 are doing, change on your own risc. DO NOT CHANGE THE DEFAULT VALUE, only use
 -DDRIVERTASKDELAY=150
 or some other value
 */
 
 #ifndef DRIVERTASKDELAY
-#define DRIVERTASKDELAY 250
+#define DRIVERTASKDELAY 100
 #endif
 
 #ifndef PUFREELISTACTIONDELAY
