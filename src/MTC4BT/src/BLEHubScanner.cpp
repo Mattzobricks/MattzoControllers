fd14228188114e4836113eaddf203b0a1904660c
Hilbert70 <hilbert70@gmail.com>
2025-02-08 20:34:19 +0100

    pr #133 more cleanup

--- src/MTC4BT/src/BLEHubScanner.cpp
+++ src/MTC4BT/src/BLEHubScanner.cpp
@@ -17,10 +17,7 @@ BLEHubScanner::BLEHubScanner()
 	// _scanner->setInterval(97);       // How often the scan occurs / switches channels; in milliseconds.
 	// _scanner->setWindow(37);         // How long to scan during the interval; in milliseconds.
 	// _scanner->setMaxResults(20);     // The number of results to limit storage to (0 = do not store the scan results, use callback only).
-	//_scanner->setDuplicateFilter(true); // Set that the BLE controller should only report results from devices it has not already seen.
-
-	//_scanner->setActiveScan(false);
-	_scanner->setDuplicateFilter(true);
+	_scanner->setDuplicateFilter(true); // Set that the BLE controller should only report results from devices it has not already seen.
 }
 
 void BLEHubScanner::StartDiscovery(std::vector<BLEHub *> &hubs, const uint32_t scanDurationInMS)
@@ -34,7 +31,6 @@ void BLEHubScanner::StartDiscovery(std::vector<BLEHub *> &hubs, const uint32_t s
 
 	log4MC::vlogf(LOG_INFO, "BLE : Scanning for %u hub(s)...", hubs.size());
 
-	// TODO: We could start using the whitelist, but then we'd have to manage it too.
 	for (BLEHub *hub : hubs) {
 		BLEDevice::whiteListAdd(hub->GetAddress());
 	}
